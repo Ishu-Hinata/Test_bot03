05ea8058405a0ea440d00a7d51473d5665706703
Lord dsp <87901742+Lord-DSP3@users.noreply.github.com>
2022-05-12 18:36:40 +0530

    Update permission.py

--- Yukki/Decorators/permission.py
+++ Yukki/Decorators/permission.py
@@ -14,23 +14,27 @@ def PermissionCheck(mystic):
                 + "\n- **can_manage_voice_chats:** To manage voice chats"
                 + "\n- **can_delete_messages:** To delete Bot's Searched Waste"
                 + "\n- **can_invite_users**: For inviting assistant to chat."
+                await app.send_sticker(message.chat.id,"CAACAgEAAx0CWu9UpwABHz_tYnz-KhmcOXLNVXdTD_bwR7vd5q4AAtMBAAKcg-hHW5ettUjivtEkBA")
             )
         if not a.can_manage_voice_chats:
             await message.reply_text(
                 "I don't have the required permission to perform this action."
                 + "\n**Permission:** __MANAGE VOICE CHATS__"
+                await app.send_sticker(message.chat.id,"CAACAgEAAx0CWu9UpwABH0AVYn0Eqd5JjWyglYJ_hTnIunT3sXEAAk0DAAITxulHgwa4nEH6gUQkBA")
             )
             return
         if not a.can_delete_messages:
             await message.reply_text(
                 "I don't have the required permission to perform this action."
                 + "\n**Permission:** __DELETE MESSAGES__"
+                await app.send_sticker(message.chat.id,"CAACAgEAAx0CWu9UpwABH0AfYn0FjFN6TQqP4RjZ6z4ibe7bI_MAApICAAKYSOlHvd1h3OfYxA4kBA")
             )
             return
         if not a.can_invite_users:
             await message.reply_text(
                 "I don't have the required permission to perform this action."
                 + "\n**Permission:** __INVITE USERS VIA LINK__"
+                await app.send_sticker(message.chat.id,"CAACAgEAAx0CWu9UpwABH0AkYn0GCihMa8Sgmk5xJ1SThPDbR_0AAooCAAJ-5-FHsLJ4QxovBbgkBA")
             )
             return
         return await mystic(_, message)
