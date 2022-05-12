03f7f4fcc519e06c82a27839360612869380a29e
Lord dsp <87901742+Lord-DSP3@users.noreply.github.com>
2022-05-12 20:18:50 +0530

    Update permission.py

--- Yukki/Decorators/permission.py
+++ Yukki/Decorators/permission.py
@@ -9,7 +9,7 @@ def PermissionCheck(mystic):
             return await mystic(_, message)
         a = await app.get_chat_member(message.chat.id, BOT_ID)
         if a.status != "administrator":
-             await message.reply_text(
+    await message.reply_text(
                 "I need to be admin with some permissions:\n"
                 + "\n- **can_manage_voice_chats:** To manage voice chats"
                 + "\n- **can_delete_messages:** To delete Bot's Searched Waste"
@@ -18,23 +18,24 @@ def PermissionCheck(mystic):
            return await app.send_sticker(message.chat.id,"CAACAgEAAx0CWu9UpwABHz_tYnz-KhmcOXLNVXdTD_bwR7vd5q4AAtMBAAKcg-hHW5ettUjivtEkBA")
             
         if not a.can_manage_voice_chats:
-            await message.reply_text(
+    await message.reply_text(
                 "I don't have the Fackin permission to perform this action....Just give Admin rights alredy (ノ｀Д´)"
                 + "\n**Permission:** __MANAGE VOICE CHATS__"
             )
-                await app.send_sticker(message.chat.id,"CAACAgEAAx0CWu9UpwABH0AVYn0Eqd5JjWyglYJ_hTnIunT3sXEAAk0DAAITxulHgwa4nEH6gUQkBA")
+            await app.send_sticker(message.chat.id,"CAACAgEAAx0CWu9UpwABH0AVYn0Eqd5JjWyglYJ_hTnIunT3sXEAAk0DAAITxulHgwa4nEH6gUQkBA")
             
             return
         if not a.can_delete_messages:
-            await message.reply_text(
+    await message.reply_text(
                 "I don't have the Fackin required permission to perform this action...Give right (>.<)"
                 + "\n**Permission:** __DELETE MESSAGES__"
             )
-                await app.send_sticker(message.chat.id,"CAACAgEAAx0CWu9UpwABH0AfYn0FjFN6TQqP4RjZ6z4ibe7bI_MAApICAAKYSOlHvd1h3OfYxA4kBA")
+             await app.send_sticker(message.chat.id,"CAACAgEAAx0CWu9UpwABH0AfYn0FjFN6TQqP4RjZ6z4ibe7bI_MAApICAAKYSOlHvd1h3OfYxA4kBA")
             
             return
         if not a.can_invite_users:
-            await message.reply_text(
+           
+    await message.reply_text(
                 "I don't have the Fackin required permission to Invite my Music Assistant. ( ･ั﹏･ั)"
                 + "\n**Permission:** __INVITE USERS VIA LINK__"
             )
