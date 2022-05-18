5806e4c9bf554f8cd5d69cf9b00a70b669532a79
Lord dsp <87901742+Lord-DSP3@users.noreply.github.com>
2022-05-18 14:03:47 +0530

    Update Play.py

--- Yukki/Plugins/Play.py
+++ Yukki/Plugins/Play.py
@@ -233,12 +233,8 @@ async def Music_Stream(_, CallbackQuery):
         )
     await CallbackQuery.answer(f"Processing:- {title[:20]}", show_alert=True)
     mystic = await CallbackQuery.message.reply_text(
-
         f"**{MUSIC_BOT_NAME} Downloader**\n\n**Title:** {title[:50]}\n\n0% ▓▓▓▓▓▓▓▓▓▓▓▓ 100%"
-
     )
-
-
     downloaded_file = await loop.run_in_executor(
         None, download, videoid, mystic, title
     )
