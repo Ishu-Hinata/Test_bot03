0406fc8486c99bf7e80eaa5ad2820363ca0874fe
Lord dsp <87901742+Lord-DSP3@users.noreply.github.com>
2022-05-18 19:00:57 +0530

    Update Start.py

--- Yukki/Plugins/Start.py
+++ Yukki/Plugins/Start.py
@@ -113,9 +113,8 @@ async def settings(_, message: Message):
     await asyncio.gather(
         message.delete(),
         message.reply_photo(
-        photo="https://telegra.ph/file/8cb1eb9ed16c68afe1994.jpg"),
-        caption=
-            f"{text}\n\n**Group:** {message.chat.title}\n**Group ID:** {message.chat.id}\n**Volume Level:** {volume}%"),
+        photo="https://telegra.ph/file/8cb1eb9ed16c68afe1994.jpg",
+        caption=f"{text}\n\n**Group:** {message.chat.title}\n**Group ID:** {message.chat.id}\n**Volume Level:** {volume}%",
             reply_markup=InlineKeyboardMarkup(buttons),
         ),
     )
