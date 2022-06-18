8e1d59c7a8a416b425e50861c3485a4ef884c535
Ishu <87901742+Ishu-Hinata@users.noreply.github.com>
2022-06-18 20:00:21 +0530

    Update __main__.py

--- Yukki/__main__.py
+++ Yukki/__main__.py
@@ -530,3 +530,31 @@ async def help_button(client, query):
 
 if __name__ == "__main__":
     loop.run_until_complete(initiate_bot())
+    
+async def bot_sys_stats():
+    bot_uptime = int(time.time() - boottime)
+    cpu = psutil.cpu_percent(interval=0.5)
+    mem = psutil.virtual_memory().percent
+    disk = psutil.disk_usage("/").percent
+    stats = f"""
+Uptime: {get_readable_time((bot_uptime))}
+CPU: {cpu}%
+RAM: {mem}%
+Disk: {disk}%"""
+    return stats
+
+
+@app.on_message(filters.command(["dsp", f"dspp@{BOT_USERNAME}"]))
+async def ping(_, message):
+    start = datetime.now()
+    response = await message.reply_photo(
+        photo="Utils/Query.jpg",
+        caption=">> Pong!",
+    )
+    uptime = await bot_sys_stats()
+    end = datetime.now()
+    resp = (end - start).microseconds / 1000
+    await response.edit_text(
+        f"**Pong!**\n`⚡{resp} ms`\n\n<b><u>{MUSIC_BOT_NAME} System Stats:</u></b>{uptime}"
+    )
+
