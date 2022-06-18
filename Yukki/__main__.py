f9efa06bfefeb18c85aba2a1bffa131981285891
Ishu <87901742+Ishu-Hinata@users.noreply.github.com>
2022-06-18 20:39:18 +0530

    Update __main__.py

--- Yukki/__main__.py
+++ Yukki/__main__.py
@@ -7,12 +7,6 @@ import time
 from datetime import datetime
 from sys import version as pyver
 
-import psutil
-from pyrogram import Client, filters
-from pyrogram.types import Message
-
-from Yukki import BOT_USERNAME, MUSIC_BOT_NAME, app, boottime
-from Yukki.Utilities.ping import get_readable_time
 
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
@@ -530,31 +524,3 @@ async def help_button(client, query):
 
 if __name__ == "__main__":
     loop.run_until_complete(initiate_bot())
-    
-async def bot_sys_stats():
-    bot_uptime = int(time.time() - boottime)
-    cpu = psutil.cpu_percent(interval=0.5)
-    mem = psutil.virtual_memory().percent
-    disk = psutil.disk_usage("/").percent
-    stats = f"""
-Uptime: {get_readable_time((bot_uptime))}
-CPU: {cpu}%
-RAM: {mem}%
-Disk: {disk}%"""
-    return stats
-
-
-@app.on_message(filters.command(["dsp", f"dspp@{BOT_USERNAME}"]))
-async def ping(_, message):
-    start = datetime.now()
-    response = await message.reply_photo(
-        photo="Utils/Query.jpg",
-        caption=">> Pong!",
-    )
-    uptime = await bot_sys_stats()
-    end = datetime.now()
-    resp = (end - start).microseconds / 1000
-    await response.edit_text(
-        f"**Pong!**\n`⚡{resp} ms`\n\n<b><u>{MUSIC_BOT_NAME} System Stats:</u></b>{uptime}"
-    )
-
